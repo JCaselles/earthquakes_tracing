087e88f415892ed608c558288a546c3e399f14f3
Arnau Orriols <dev@arnauorriols.com>
2013-10-04 21:14:44 +0200

    Applies some changes to prototype

--- prototype.py
+++ prototype.py
@@ -8,17 +8,17 @@ from lxml.html import document_fromstring
 
 BASE_URL = 'http://www.ign.es/ign/layoutIn/sismoListadoTerremotos.do'
 
-def get_earthquakes_list():
+def get_earthquakes_list(list_days=5):
 
-    payload = {'zona' : '1', 'cantidad_dias' : 5}
+    payload = {'zona' : '1', 'cantidad_dias' : list_days}
 
     web_content = document_fromstring(get(BASE_URL, params=payload).text)
 
     eq_table = web_content.cssselect('tr.filaNegra2')
 
     eq_list = []
-    for row in eq_list:
-        eq_list.appned({'date' : row[1].text,
+    for row in eq_table:
+        eq_list.append({'date' : row[1].text,
                         'hour' : row[2].text,
                         'magnitude' : row[7].text,
                         'location' : row[9].text
