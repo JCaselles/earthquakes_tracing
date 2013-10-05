214db35d51dd8a867dfcc901360c32d1adefccaf
Arnau Orriols <dev@arnauorriols.com>
2013-10-05 16:11:08 +0200

    Adds jsoup library. Replaces httpUrlConnection for JSoup builtin connection feature

--- AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/RequestHelper.java
+++ AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/RequestHelper.java
@@ -18,11 +18,20 @@ import android.content.Context;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import org.jsoup.helper.*;
+import org.jsoup.parser.*;
+import org.jsoup.safety.*;
 
 class RequestHelper {
 
@@ -47,50 +56,78 @@ class RequestHelper {
         }
     }
 
-    public HashMap<String, String> fetchEarthquakeList(int listDays) throws IOException{
+    public List<HashMap<String, String>> fetchEarthquakeList(int listDays) throws IOException{
 
-        InputStream is = null;
+        //InputStream is = null;
 
-        try {
-                URL url = new URL(BASE_URL);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
+        //try {
+        //        URL url = new URL(BASE_URL);
+        //        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        //        conn.setReadTimeout(10000 /* milliseconds */);
+        //        conn.setConnectTimeout(15000 /* milliseconds */);
+        //        conn.setRequestMethod("GET");
+        //        conn.setDoInput(true);
                 // Starts the query
-                conn.connect();
-                int response = conn.getResponseCode();
-                Log.d(DEBUG_TAG, "The response is: " + response);
-                is = conn.getInputStream();
+        //        conn.connect();
+        //        int response = conn.getResponseCode();
+        //        Log.d(DEBUG_TAG, "The response is: " + response);
+        //        is = conn.getInputStream();
 
                 // Convert the InputStream into a string
-                String contentAsString = readIt(is);
+        //        String contentAsString = readIt(is);
 
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
-            } finally {
-                if (is != null) {
-                    is.close();
-                } 
-            }
+        //    } finally {
+        //        if (is != null) {
+        //            is.close();
+        //        } 
+        //    }
+        //
+        //    THIS IS RATHER TOO COMPLEX, OLD-FASHION. WILL TRY TO USE
+        //    JSOUP BUILTIN CONNECTION FEATURE. KEEPING IT FOR
+        //    EDUCATIONAL PURPOSES.
 
-        HashMap<String, String> hs = new HashMap<String, String>();
-        return hs;
-    }
 
-    private String readIt(InputStream is) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String result = null;
-        String line = null;
-        try{
-            while ((line = br.readLine()) != null){
-                result += line;
+        HashMap<String, String> queryString = new HashMap<String, String>();
+        queryString.put("zona", "1");
+        queryString.put("cantidad_dias", Integer.toString(listDays));
+        List<HashMap<String, String>> eqList = new ArrayList<HashMap<String, String>>();
+        if (checkNetwork()){
+            try{
+                Document webContent = Jsoup.connect(BASE_URL).data(queryString).get();
+                Elements eqTableRows = webContent.select("tr.filaNegra2");
+                for (Element row : eqTableRows){
+                    Elements children = row.children();
+                    HashMap<String, String> eqData = new HashMap<String, String>();
+                    eqData.put("date", children.get(1).text());
+                    eqData.put("time", children.get(2).text());
+                    eqData.put("magnitude", children.get(7).text());
+                    eqData.put("location", children.get(9).text());
+                    eqList.add(eqData);
+                }
+            }catch (IOException e){
+                Log.e(DEBUG_TAG, "Error connecting with Jsoup to " + BASE_URL);
+            }finally{
+                return eqList;
             }
-        }finally{
-            return result;
+        }else{
+            return null;
         }
     }
+
+    //private String readIt(InputStream is) throws IOException{
+    //    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    //    String result = null;
+    //    String line = null;
+    //    try{
+    //        while ((line = br.readLine()) != null){
+    //            result += line;
+    //        }
+    //    }finally{
+    //        return result;
+    //    }
+    //}
 
 
     }
