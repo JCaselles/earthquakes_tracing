0c8c21ecff457da6cff4152cf98d991708cc39ae
Arnau Orriols <dev@arnauorriols.com>
2013-10-22 02:08:40 +0200

    Comments significant and problematic methods. Applies line width convention

--- AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TerremotosSeguimiento.java
+++ AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TerremotosSeguimiento.java
@@ -22,47 +22,80 @@ import com.google.analytics.tracking.android.EasyTracker;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public class TerremotosSeguimiento extends ActionBarActivity implements TSListFragment.OnRowSelectedListener
-{
+public class TerremotosSeguimiento extends ActionBarActivity
+                            implements TSListFragment.OnRowSelectedListener {
+
     private static final int MAX_ITEMS = 2;
     private TSFragmentPageAdapter tsfpa;
     private ViewPager vp;
     private TSAlarmReceiver alarm = new TSAlarmReceiver();
     private boolean activated = false;
     private TSFileLoader tsfl;
-    /** Called when the activity is first created. */
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        activated = (PendingIntent.getBroadcast(this, 0, new Intent(this, TSAlarmReceiver.class), PendingIntent.FLAG_NO_CREATE) != null);
-        Log.d(RequestHelper.DEBUG_TAG, "activated is" + String.valueOf(activated));
+        activated = (PendingIntent.getBroadcast(
+                this, 0, new Intent(this, TSAlarmReceiver.class),
+                        PendingIntent.FLAG_NO_CREATE) != null);
 
+        Log.d(RequestHelper.DEBUG_TAG, "activated is" +
+                                                    String.valueOf(activated));
+
+        // TSFragmentPageAdapter is an inner class of this. it defines the
+        // instantation of each fragment for each tab.
         tsfpa = new TSFragmentPageAdapter(this, getSupportFragmentManager());
         vp = (ViewPager) findViewById(R.id.pager);
         vp.setAdapter(tsfpa);
+
         ActionBar ab = getSupportActionBar();
         ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // TabListener calls the ViewPager to change the current position based
+        // on the current selected tab. This links the selection of a tab to
+        // the ViewPager.
         ActionBar.TabListener tl = new ActionBar.TabListener() {
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft){
+
+            public void onTabSelected(ActionBar.Tab tab,
+                                      FragmentTransaction ft){
                 vp.setCurrentItem(tab.getPosition());
             }
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft){
-            }
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft){
-            }
+
+            public void onTabUnselected(ActionBar.Tab tab,
+                                        FragmentTransaction ft){}
+
+            public void onTabReselected(ActionBar.Tab tab,
+                                        FragmentTransaction ft){}
         };
 
-        vp.setOnPageChangeListener( new ViewPager.SimpleOnPageChangeListener() {
+        // Changes de selected tab on the Action Bar based on the selected page
+        // of the ViewPager. This links the ViewPager swipe with the Action Bar
+        // tabs.
+        vp.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
                 getSupportActionBar().setSelectedNavigationItem(position);
             }
         });
 
-        ab.addTab(ab.newTab().setText(getString(R.string.list_tab_label)).setTabListener(tl));
-        ab.addTab(ab.newTab().setText(getString(R.string.details_tab_label)).setTabListener(tl));
+        // Adds the two tabs to the Action Bar. Each tab gets a label and the
+        // tab listener, which links the tab to the ViewPager.
+        ab.addTab(ab.newTab()
+            .setText(getString(R.string.list_tab_label))
+            .setTabListener(tl)
+        );
+        ab.addTab(ab.newTab()
+            .setText(getString(R.string.details_tab_label))
+            .setTabListener(tl)
+        );
+
+        // If no EQDATA intent, loads eqList from file or url. If EQDATA intent
+        // is found, updates eqList from the intent.
+        // TODO: Should be improved, sometimes gets stuck, sometimes shows
+        // wrong data.
         if(new RequestHelper(this).checkNetwork()){
             if (!getIntent().hasExtra(TSService.EQ_DATA)){
                 tsfl = new TSFileLoader(this);
@@ -71,9 +104,13 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
                 importEQDATAIntent(getIntent());
             }
         }
-
     }
 
+
+    /**
+     * Cancels TSFileLoader asynctask if not finished when the activity is
+     * destroyed.
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -97,14 +134,24 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
     }
 
 
+    /**
+     * Updates eqList extracting it from EQ_DATA intent. Changes to details
+     * tab automatically, calling setCurrentItem method of the ViewPager.
+     */
     public void importEQDATAIntent(Intent intent){
-        ArrayList<HashMap<String, String>> eqData = (ArrayList<HashMap<String, String>>) intent.getSerializableExtra(TSService.EQ_DATA);
+        ArrayList<HashMap<String, String>> eqData =
+            (ArrayList<HashMap<String, String>>)
+                            intent.getSerializableExtra(TSService.EQ_DATA);
         TSListFragment.updateEqList(eqData);
         resetViewPager();
-        vp.setCurrentItem(1); // TOFIX: Causes crash when app been created with EQDATA intent -> fragment returns nullPointerException
+        // TOFIX: Causes crash when app been created with EQDATA
+        // intent -> fragment returns nullPointerException
+        vp.setCurrentItem(1);
     }
 
 
+    /** PageAdapter is destroyed and rebuild to update the fragments data. */
+    // TODO: Consider notifyDataSetChanged, it should work.
     public void resetViewPager(){
         //vp.getAdapter().notifyDataSetChanged();
         vp.setAdapter(null);
@@ -112,11 +159,18 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
         Log.v(RequestHelper.DEBUG_TAG, "List should be updated");
     }
 
+
+    /**
+     * This is called when a new intent is delivered with an instance of this
+     * activity already present. This is due to the singleTop attribute of the
+     * activity.
+     */
     @Override
     protected void onNewIntent(Intent intent){
         setIntent(intent);
         importEQDATAIntent(intent);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -130,11 +184,11 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
         return true;
     }
 
+
     // Menu options to set and cancel the alarm.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            // When the user clicks START ALARM, set the alarm.
             case R.id.activation_switch:
                 if(!activated){
                     alarm.setAlarm(this);
@@ -152,6 +206,8 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
     public static class TSFragmentPageAdapter extends FragmentPagerAdapter {
         private Context context;
         public TSFragmentPageAdapter(Context context, FragmentManager fm) {
@@ -169,24 +225,28 @@ public class TerremotosSeguimiento extends ActionBarActivity implements TSListFr
             Fragment f = null;
             switch (position) {
                 case 0:
-                    f = Fragment.instantiate(context, TSListFragment.class.getName());
+                    f = Fragment.instantiate(context,
+                                             TSListFragment.class.getName());
                     break;
                 case 1:
-                    f = Fragment.instantiate(context, TSDetailsFragment.class.getName());
+                    f = Fragment.instantiate(context,
+                                        TSDetailsFragment.class.getName());
                     break;
             }
             return f;
         }
     }
 
+
     @Override
     public void onRowSelected(HashMap<String, String> eqData){
-        ArrayList<HashMap<String, String>> eqList = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> eqList =
+                                new ArrayList<HashMap<String, String>>();
         eqList.add(eqData);
-        Intent intent = new Intent().putExtra(TSListFragment.INTENT_DETAILS, eqList);
+
+        Intent intent = new Intent()
+                        .putExtra(TSListFragment.INTENT_DETAILS, eqList);
         setIntent(intent);
         vp.setCurrentItem(1);
     }
 }
-
-
