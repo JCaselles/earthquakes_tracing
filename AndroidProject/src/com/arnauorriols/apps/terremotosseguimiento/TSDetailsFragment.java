cd42b3d104b9f6bcf8dc4371d5050247fc099434
Arnau Orriols <dev@arnauorriols.com>
2013-11-01 20:54:45 +0100

    Adds missing import statement

--- AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSDetailsFragment.java
+++ AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSDetailsFragment.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
