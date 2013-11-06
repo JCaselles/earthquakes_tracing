37fc113da677169e49cb81deb24a9462253c9ce0
Arnau Orriols <dev@arnauorriols.com>
2013-11-06 02:24:05 +0100

    Comments and fixes code layout of TSAlarmReceiver. Tries removing DONT_KILL_APP flag

--- AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSAlarmReceiver.java
+++ AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSAlarmReceiver.java
@@ -3,7 +3,7 @@
  *
  * Provides repeating alarm scheduling feature to Terremotos
  * Seguimiento. This class handles the alarm scheduling for firing the
- * IntentService every 15 minutes aprox. 
+ * IntentService every 15 minutes aprox.
  *
  * Author: Arnau Orriols
  */
@@ -19,52 +19,82 @@ import android.content.Intent;
 import android.util.Log;
 import android.os.SystemClock;
 
+
+/**
+ * Implementation of the abstract class WakefulBroadcastReceiver, which is a
+ * helper to start a service that ensures the devices awakes and doesn't go
+ * back to sleep. It works as an alarm shedule: sets the time rate for a new
+ * intent to get fired by AlarmManager (sort of cronjob) which is recived by
+ * this same BroadcastReceiver, which in turn builds a new intent and starts a
+ * new services with this intent, even if the device is sleep.
+ *
+ * It implements the code for setting this alarm at device boot.
+ */
 public class TSAlarmReceiver extends WakefulBroadcastReceiver {
 
-    private AlarmManager am;
-    private PendingIntent pi;
+    //AlarmManager am;
+    //private PendingIntent pi;
 
+    /** Creates a new intent to start TSService service */
     @Override
     public void onReceive(Context context, Intent intent){
-        Log.v(RequestHelper.DEBUG_TAG, "Service is fired");
         Intent service = new Intent(context, TSService.class);
         startWakefulService(context, service);
-
     }
 
+    /**
+     * Configures the alarm to fire once every 15 minutes (inexactRepeating).
+     * AlarmManager will send a broadcast PendingIntent to this same class,
+     * which will start the service when received. It also sets this alarm to
+     * be set on device boot, using TSBootReceiver BroadcastReceiver.
+     */
     public void setAlarm(Context context){
-        am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+        AlarmManager am =
+                (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
         Intent intent = new Intent (context, TSAlarmReceiver.class);
-        pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
         am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                                SystemClock.elapsedRealtime() + 10 * 1000,
                                AlarmManager.INTERVAL_FIFTEEN_MINUTES,
                                pi);
-        ComponentName receiver = new ComponentName(context, TSBootReceiver.class);
-        PackageManager pm = context.getPackageManager();
 
+        /* Enabling this alarm to be set in every device boot */
+        ComponentName receiver = new ComponentName(context,
+                                                   TSBootReceiver.class);
+        PackageManager pm = context.getPackageManager();
         pm.setComponentEnabledSetting(receiver,
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                PackageManager.DONT_KILL_APP);
-        Log.v(RequestHelper.DEBUG_TAG, "Alarm is setted");
+                0);  // TODO: Trying without DONT_KILL_APP
+        Log.v(RequestHelper.DEBUG_TAG, "Alarm is set");
     }
+
+
+    /**
+     * Gets any existing PendingIntents, it there is any means the alarm is
+     * activated, cancelling it. It also cancels the alarm on device boot.
+     *
+     * TODO: Consider using settings instead, as they are going to be used
+     * anyway.
+     */
     public void cancelAlarm(Context context) {
-        // If the alarm has been set, cancel it.
         Intent intent = new Intent (context, TSAlarmReceiver.class);
-        pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent,
+                                                PendingIntent.FLAG_NO_CREATE);
+
+        /* pi will be null if there isn't any PedingIntent with this intent */
         if (pi != null) {
             Log.v(RequestHelper.DEBUG_TAG, "Intent found, canceling alarm");
-            am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+            AlarmManager am = (AlarmManager) context.getSystemService(
+                                                        context.ALARM_SERVICE);
             am.cancel(pi);
-            if (PendingIntent.getBroadcast(context, 0, new Intent(context, TSAlarmReceiver.class), PendingIntent.FLAG_NO_CREATE) == null) {
-                Log.v(RequestHelper.DEBUG_TAG, "Alarm successfully canceled");
-            }
-            ComponentName receiver = new ComponentName(context, TSBootReceiver.class);
-            PackageManager pm = context.getPackageManager();
 
+            /* Cancel the alarm to be set on every device boot */
+            ComponentName receiver = new ComponentName(context,
+                                                       TSBootReceiver.class);
+            PackageManager pm = context.getPackageManager();
             pm.setComponentEnabledSetting(receiver,
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                    PackageManager.DONT_KILL_APP);
+                    0); // TODO: Trying without DONT_KILL_APP
         }
     }
 }
