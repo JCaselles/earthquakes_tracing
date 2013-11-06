521597310a9f153d3dad9de6260ceddfcbd94b15
Arnau Orriols <dev@arnauorriols.com>
2013-11-06 14:09:01 +0100

    Comments and corrects code layout of TSFileLoader

--- AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSFileLoader.java
+++ AndroidProject/src/com/arnauorriols/apps/terremotosseguimiento/TSFileLoader.java
@@ -1,10 +1,19 @@
+/*
+ * TSFileLoader.java
+ *
+ * Implementation of an AsyncTask to load the earthquakes list when the app is
+ * started.
+ *
+ * Author: Arnau Orriols
+ *
+ * Copyright 2013 Arnau Orriols.
+ */
 package com.arnauorriols.apps.terremotosseguimiento;
 
-import android.content.Context;
-import android.widget.Toast;
-import android.util.Log;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+import android.content.Context;
+import android.util.Log;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
@@ -12,7 +21,16 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public class TSFileLoader extends AsyncTask<Void, Void, ArrayList<HashMap<String, String>>>{
+
+/**
+ * Implementation of AsyncTask to load from file or url the earthquakes' list.
+ * If there is an existing LIST_LATEST in the app's system directory, it is
+ * read. If the file doesn't exist yet, this is is feched from internet using
+ * RequestHelper class. When done, EqList is updated and the ViewPager that
+ * hostes the ListFragment is reseted to show the results.
+ */
+public class TSFileLoader extends AsyncTask<Void, Void, ArrayList
+                                                <HashMap<String, String>>>{
 
     private Context context;
 
@@ -20,6 +38,10 @@ public class TSFileLoader extends AsyncTask<Void, Void, ArrayList<HashMap<String
         this.context = context;
     }
 
+    /**
+     * Reads from LIST_LATEST file, or fetches from url if file doesn't exist.
+     * Returns the list of HashMaps with the data of all the earthquakes.
+     */
     public ArrayList<HashMap<String, String>> loadEarthquakesList(){
         ArrayList<HashMap <String, String>> eqList = null;
         FileInputStream fis = null;
@@ -33,7 +55,9 @@ public class TSFileLoader extends AsyncTask<Void, Void, ArrayList<HashMap<String
             eqList = new RequestHelper(context).fetchEarthquakeList(2);
         }catch (Exception e){
             Log.e(RequestHelper.DEBUG_TAG, "Error: ", e);
-            Toast toast = Toast.makeText(context, context.getString(R.string.unknown_error), Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(context,
+                    context.getString(R.string.unknown_error),
+                                                    Toast.LENGTH_LONG);
         }
         return eqList;
     }
@@ -41,12 +65,10 @@ public class TSFileLoader extends AsyncTask<Void, Void, ArrayList<HashMap<String
     protected ArrayList<HashMap<String, String>> doInBackground(Void... v) {
         return loadEarthquakesList();
     }
-
     protected void onProgressUpdate(Void... v){}
     protected void onPostExecute(ArrayList<HashMap<String, String>> result){
         TSListFragment.updateEqList(result);
         ((TerremotosSeguimiento)context).resetViewPager();
     }
-
 }
 
